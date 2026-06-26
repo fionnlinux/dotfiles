d201623d751f39485b35a0e799415cb1d9d5ef45
fionnlinux <251637233+fionnlinux@users.noreply.github.com>
2026-06-26 16:56:46 -0400

    fix: update canary path to projects folder

--- aliases/tibia.sh
+++ aliases/tibia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # OpenTibia / Canary family LAN server shortcuts
 
-alias tibia-lan='cd ~/open-tibia/canary/docker && LAN=true sh ./up.sh'
-alias tibia-stop='cd ~/open-tibia/canary/docker && docker compose down'
-alias tibia-logs='cd ~/open-tibia/canary/docker && docker compose logs -f server'
-alias tibia-status='cd ~/open-tibia/canary/docker && docker compose ps'
+alias tibia-lan='cd ~/projects/canary/docker && LAN=true sh ./up.sh'
+alias tibia-stop='cd ~/projects/canary/docker && docker compose down'
+alias tibia-logs='cd ~/projects/canary/docker && docker compose logs -f server'
+alias tibia-status='cd ~/projects/canary/docker && docker compose ps'
